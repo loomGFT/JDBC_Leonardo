d2c4447ebc8c7ffbe2ff2a5edd9b769842140b35
Administrator <loom@gft.com>
2022-05-20 10:57:46 +0200

    Project creation + JDBC + DBCreation File

---
+++ src/test/java/com/example/jdbc_leonardo/JdbcLeonardoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbc_leonardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcLeonardoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
